d081a63c407ddc6877bc72de3934ab157adad737
Aryan Patil <125805392+aaryan-exe@users.noreply.github.com>
2025-02-08 14:44:16 +0530

    new

--- CineGo/HomePage/HomePage.aspx.cs
+++ CineGo/HomePage/HomePage.aspx.cs
@@ -12,6 +12,7 @@ namespace CineGo.HomePage
         protected void Page_Load(object sender, EventArgs e)
         {
             nametextbox.Text = Session["uemail"].ToString();
+
         }
     }
 }
